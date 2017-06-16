e7a2f2c5591331f0aa841bd8387a773df4b99c62
SueNobuhiro <>
2017-06-16 10:25:01 +0900

    mofify to cause intentional failure

--- server.js
+++ server.js
@@ -72,7 +72,8 @@ app.get('/', function (req, res) {
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    // uncomment below
+    //res.render('index.html', { pageCountMessage : null});
   }
 });
 
